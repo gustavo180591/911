775e18e519a2ebf14f61f7c8f5a4dc9b0384c316
gustavo180591 <gustavo.faccendini@gmail.com>
2025-01-29 09:44:56 -0300

    save changes

--- src/Controller/EmergencyController.php
+++ src/Controller/EmergencyController.php
@@ -225,3 +225,10 @@ public function reject(int $id, Request $request, EntityManagerInterface $entity
 }
 
 }
+
+// la fecha y hora que vaya al momento de la denuncia
+// los datos del denunciante que figuren en la denuncia
+// foto del dni, selfie del denunciante para el registro y validar usuario.
+// si se acepta o no la denuncia, se envia un correo al denunciante con la respuesta.
+// agregar filto
+// Cargá imágenes del frente y del dorso de tu DNI, cédula o libreta cívica
