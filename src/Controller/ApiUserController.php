dcc1154b4868d6efac61df8f45741a017eb1b1d8
gustavo180591 <gustavo.faccendini@gmail.com>
2025-05-04 21:45:41 -0300

    add API endpoint to check user existence by phone number

---
+++ src/Controller/ApiUserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiUserController extends AbstractController
+{
+    #[Route('/api/check-phone', name: 'api_check_phone', methods: ['POST'])]
+    public function checkPhone(Request $request, UserRepository $userRepository): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        if (!isset($data['telefono'])) {
+            return $this->json(['error' => 'Falta el campo telefono'], 400);
+        }
+
+        $telefono = trim($data['telefono']);
+        if ($telefono === '') {
+            return $this->json(['error' => 'El teléfono no puede estar vacío'], 400);
+        }
+
+        $user = $userRepository->findOneBy(['telefono' => $telefono]);
+        if ($user) {
+            return $this->json(['id' => $user->getId()], 200);
+        } else {
+            return $this->json(['error' => 'Usuario no encontrado'], 404);
+        }
+    }
+} 
